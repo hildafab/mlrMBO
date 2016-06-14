aaaba5b09bdf3d3c3eff28218f6dd960fd7d6e94
janek <janek.thomas@web.de>
2016-06-14 23:30:38 +0200

    rng only on CRAN and removed run-all

--- tests/testthat/helper_zzz.R
+++ tests/testthat/helper_zzz.R
@@ -1,4 +1,10 @@
-if (!identical(Sys.getenv("TRAVIS"), "true"))
+if (!is.na(Sys.getenv("NOT_CRAN", unset = NA))) {
   set.seed(1)
+} else {
+  seed.val = sample(1:100, size = 1)
+  set.seed(seed.val)
+  catf("Run test with seed: %i", seed.val)
+}
+
 configureMlr(show.learner.output = FALSE)
 options(mlrMBO.show.info = FALSE, mlrMBO.debug.mode = TRUE)
