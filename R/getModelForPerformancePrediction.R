9488bbfcfb82ae7c36254400b36834983bf70e97
Janek <janek.thomas@web.de>
2016-12-06 12:41:28 +0100

    code cleanup

--- R/getModelForPerformancePrediction.R
+++ R/getModelForPerformancePrediction.R
@@ -1,8 +1,8 @@
-getModelForPerformancePrediction <- function(learner){
+getModelForPerformancePrediction = function(learner){
   #   cat("Getting the prediction model for learner : ",learner,"\n")
-  lrn <- makeLearner(learner)
-  vname <- load(file=paste('R/models/',paste(lrn$short.name,'model.RData',sep = '_'),sep = ''))
-  model <- get(vname)
+  lrn = makeLearner(learner)
+  vname = load(file=paste('R/models/',paste(lrn$short.name,'model.RData',sep = '_'),sep = ''))
+  model = get(vname)
   #   print("Obtained prediction model")
   return(model)
 }
--- R/infill_crits_metambo.R
+++ R/infill_crits_metambo.R
@@ -20,14 +20,14 @@ infillCritEIMtL = function(points, models, control, par.set, design, iter) {
   #   print("inside infill crit eimtl")
   #   print(openml.did)
   #get meta features
-  dataset.metafeatures <- getMetaDataForDataset(did=openml.did)
-  parset.with.trafo <- control$infill.crit.eimtl.parset
+  dataset.metafeatures = getMetaDataForDataset(did=openml.did)
+  parset.with.trafo = control$infill.crit.eimtl.parset
   #get performance for the point based on the meta features and hyper parameters  
-  perf <- getPerformanceFromMetaInfo(points,dataset.metafeatures,parset.with.trafo)
-  perf <- maximize.mult * perf
+  perf = getPerformanceFromMetaInfo(points,dataset.metafeatures,parset.with.trafo)
+  perf = maximize.mult * perf
   #perf = 1
   #   options(error = browser())
-  decay <- control$eimtl.decay.param  
+  decay = control$eimtl.decay.param  
   ei = ((1 - (decay ^ iter)) * ei) + (perf * (decay ^ iter))
   
   # FIXME: magic number
