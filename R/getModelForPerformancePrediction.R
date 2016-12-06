4a9b7246a9ad7d501ef8e3f1198a546b4bfc5d6a
Janek Thomas <janek.thomas@web.de>
2016-12-06 20:51:00 +0100

    move models to data

--- R/getModelForPerformancePrediction.R
+++ R/getModelForPerformancePrediction.R
@@ -1,8 +1,6 @@
 getModelForPerformancePrediction = function(learner){
-  #   cat("Getting the prediction model for learner : ",learner,"\n")
   lrn = makeLearner(learner)
-  vname = load(file=paste('R/models/',paste(lrn$short.name,'model.RData',sep = '_'),sep = ''))
+  vname = data(lrn$short.name, "model.RData", sep = "_")
   model = get(vname)
-  #   print("Obtained prediction model")
   return(model)
 }
